d94e092769052ea600056ee254257a03f490dc19
Aman Mangal <mangalaman93@gmail.com>
2018-10-16 12:10:15 +0530

    refactor code, add support for configuring bigqueue
    This patch adds support for setting custom Arena size
    through function options. The pattern used is described here
    https://commandcenter.blogspot.com/2014/01/self-referential-functions-and-design.html.

--- utils_test.go
+++ utils_test.go
@@ -2,24 +2,19 @@ package bigqueue
 
 import (
 	"os"
-	"path"
 	"testing"
-)
-
-var (
-	testPath = path.Join(os.TempDir(), "testdir_%d")
 )
 
 func createTestDir(t *testing.T, testDir string) {
 	if _, err := os.Stat(testDir); os.IsNotExist(err) {
 		if err := os.Mkdir(testDir, cFilePerm); err != nil {
-			t.Errorf("unable to create test dir: %s", err)
+			t.Errorf("unable to create test dir: %v", err)
 		}
 	}
 }
 
 func deleteTestDir(t *testing.T, testDir string) {
 	if err := os.RemoveAll(testDir); err != nil {
-		t.Errorf("unable to delete test dir: %s", err)
+		t.Errorf("unable to delete test dir: %v", err)
 	}
 }
