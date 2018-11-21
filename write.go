e233293c5706ee48dbe644b63e02513e6c335b0e
Aman Mangal <mangalaman93@gmail.com>
2018-11-21 22:25:33 +0530

    Simplify enqueue and dequeue, fix bugs

--- write.go
+++ write.go
@@ -24,51 +24,34 @@ func (bq *BigQueue) Enqueue(message []byte) error {
 }
 
 func (bq *BigQueue) writeLength(aid, offset int, length uint64) (int, int, error) {
+	// ensure that new arena is available if needed
+	if cInt64Size+offset >= bq.arenaSize {
+		if err := bq.addNewArena(aid + 1); err != nil {
+			return 0, 0, err
+		}
+	}
+
 	// check if length can be fit into same arena, if not, get new arena
 	if cInt64Size+offset <= bq.arenaSize {
 		bq.arenaList[aid].WriteUint64(offset, length)
 	} else {
 		aid, offset = aid+1, 0
-		if err := bq.addNewArena(aid); err != nil {
-			return 0, 0, err
-		}
-
 		bq.arenaList[aid].WriteUint64(offset, length)
 	}
 
 	// update offset now
 	offset += cInt64Size
-
-	return aid, offset, nil
-}
-
-// writeBytes writes byteSlice in arena with aid starting at offset, if byteSlice size
-// is greater than arena size then it calls writeBytesToMultipleArenas
-func (bq *BigQueue) writeBytes(aid, offset int, byteSlice []byte) (int, int, error) {
-	length := len(byteSlice)
-
-	// check if slice can be fit into same aid
-	if bq.arenaSize-offset >= length {
-		if _, err := bq.arenaList[aid].Write(byteSlice, offset); err != nil {
-			return 0, 0, err
-		}
-		offset += len(byteSlice)
-	} else {
-		var err error
-		aid, offset, err = bq.writeBytesToMultipleArenas(aid, offset, byteSlice)
-		if err != nil {
-			return 0, 0, err
-		}
+	if offset == bq.arenaSize {
+		aid, offset = aid+1, 0
 	}
 
 	return aid, offset, nil
 }
 
-// writeBytesToMultipleArenas is called if byteSlice cannot be fit into single arena
-func (bq *BigQueue) writeBytesToMultipleArenas(aid, offset int, byteSlice []byte) (
-	int, int, error) {
-
+// writeBytes writes byteSlice in arena with aid starting at offset
+func (bq *BigQueue) writeBytes(aid, offset int, byteSlice []byte) (int, int, error) {
 	length := len(byteSlice)
+
 	counter := 0
 	for {
 		bytesWritten, err := bq.arenaList[aid].Write(byteSlice[counter:], offset)
@@ -76,15 +59,19 @@ func (bq *BigQueue) writeBytesToMultipleArenas(aid, offset int, byteSlice []byte
 			return 0, 0, err
 		}
 		counter += bytesWritten
+		offset += bytesWritten
 
-		// check if all bytes are written
-		if counter < length {
+		// ensure the next arena is available if needed
+		if offset == bq.arenaSize {
 			if err = bq.addNewArena(aid + 1); err != nil {
 				return 0, 0, err
 			}
+
 			aid, offset = aid+1, 0
-		} else {
-			offset = bytesWritten
+		}
+
+		// check if all bytes are written
+		if counter == length {
 			break
 		}
 	}
